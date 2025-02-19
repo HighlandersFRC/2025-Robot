cefb94ada807bd569e5033fa960cb3e0c8ff29b1
RishabhK12 <rishabhkapoor@live.com>
2025-02-18 20:25:48 -0700

    Adjusted l2 offset

--- 2025-Robot/src/main/java/frc/robot/Constants.java
+++ 2025-Robot/src/main/java/frc/robot/Constants.java
@@ -536,9 +536,9 @@ public final class Constants {
     // public static final double INTAKE_Y_OFFSET_BACK = inchesToMeters(-4.3);
 
     public static final double INTAKE_X_OFFSET_FRONT = inchesToMeters(24.5);
-    public static final double INTAKE_Y_OFFSET_FRONT = inchesToMeters(2.5);
+    public static final double INTAKE_Y_OFFSET_FRONT = inchesToMeters(3.5);
     public static final double INTAKE_X_OFFSET_BACK = inchesToMeters(24.5);
-    public static final double INTAKE_Y_OFFSET_BACK = inchesToMeters(-2.5);
+    public static final double INTAKE_Y_OFFSET_BACK = inchesToMeters(-3.5);
 
     public static final double L3_INTAKE_X_OFFSET_FRONT = inchesToMeters(29.5);
     public static final double L3_INTAKE_Y_OFFSET_FRONT = inchesToMeters(4.2);
