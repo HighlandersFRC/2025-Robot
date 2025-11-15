c86cf98effca51d5d0e56697063ee866bc24969f
Deepesh Kothari <112287069+Deepestest@users.noreply.github.com>
2025-11-15 12:31:24 -0700

    did pivot comp io

---
+++ 2025-Robot/src/main/java/frc/robot/subsystems/pivot/PivotIOSim.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems.pivot;
+
+import frc.robot.subsystems.pivot.Pivot.PivotState;
+
+public class PivotIOSim implements PivotIO {
+
+    @Override
+    public void init() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'init'");
+    }
+
+    @Override
+    public void updateInputs(PivotState systemState) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateInputs'");
+    }
+
+    @Override
+    public void setPosition(double rotations, double maxPivotDegrees, double nonAlgaeTime) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPosition'");
+    }
+
+    @Override
+    public void setPercent(double percent) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPercent'");
+    }
+
+    @Override
+    public double getPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+
+    @Override
+    public void setPositionSlow(double pivotPosition, double maxPivotDegrees) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPositionSlow'");
+    }
+
+    @Override
+    public void setPositionSlower(double pivotPosition, double maxPivotDegrees) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPositionSlower'");
+    }
+
+}
